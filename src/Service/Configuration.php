4146583d6705952368b157f6e99dfe79a488ad98
brainshaker <parupp@gmx.de>
2023-01-15 21:48:08 +0100

    docs(config): add method descriptions

--- src/Service/Configuration.php
+++ src/Service/Configuration.php
@@ -34,11 +34,19 @@ class Configuration
         $this->config = FullConfig::fromArray($config);
     }
 
+    /**
+     * Get the base configuration.
+     */
     public function get(): FullConfig
     {
         return $this->config;
     }
 
+    /**
+     * Merge the base configuration with the given configuration.
+     *
+     * @param ?Config $config the config to merge
+     */
     public function merge(?Config $config): FullConfig
     {
         if ($config instanceof FullConfig) {
