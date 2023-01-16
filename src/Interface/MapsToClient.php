0af30eb2bd551551c26c9f0169098d3ccd4470a4
brainshaker <parupp@gmx.de>
2023-01-16 21:54:01 +0100

    feat(ts-response): add validateClientData function to MapsToClient
    This should prevent arbitrary data that is not defined in the TypeScriptable instances to be mapped.
    A supplementary trait has been added that can be used if a basic implementation of this function is sufficient.

--- src/Interface/MapsToClient.php
+++ src/Interface/MapsToClient.php
@@ -2,6 +2,8 @@
 
 namespace Brainshaker95\PhpToTsBundle\Interface;
 
+use ReflectionProperty;
+
 /**
  * @phpstan-type ClientData array<string,string|int|float|bool|mixed[]|null>
  */
@@ -13,4 +15,15 @@ interface MapsToClient
      * @phpstan-return ClientData
      */
     public function mapToClient(array $properties): array;
+
+    /**
+     * @phpstan-param ClientData $data
+     *
+     * @param ReflectionProperty[] $properties
+     *
+     * @phpstan-param ClientData $data
+     *
+     * @phpstan-return ClientData
+     */
+    public function validateClientData(array $properties, array $data): array;
 }
