b2350f2bf89edaa6b1b6133c07fe3a714fc88aa2
brainshaker <parupp@gmx.de>
2023-01-23 22:30:50 +0100

    feat(indent): make indent stringable

--- src/Model/Config/Indent.php
+++ src/Model/Config/Indent.php
@@ -3,19 +3,34 @@
 namespace Brainshaker95\PhpToTsBundle\Model\Config;
 
 use Brainshaker95\PhpToTsBundle\Interface\Config as C;
+use Stringable;
 
-class Indent
+class Indent implements Stringable
 {
     public const STYLE_SPACE = 'space';
     public const STYLE_TAB   = 'tab';
 
     /**
-     * @param self::STYLE_* $style
+     * @phpstan-param self::STYLE_* $style
+     *
      * @param int<0,max> $count
      */
     public function __construct(
         public readonly string $style = C::INDENT_STYLE_DEFAULT,
         public readonly int $count = C::INDENT_COUNT_DEFAULT,
     ) {
+    }
+
+    public function __toString(): string
+    {
+        return $this->toString();
+    }
+
+    public function toString(): string
+    {
+        return str_repeat(
+            $this->style === self::STYLE_TAB ? "\t" : ' ',
+            $this->count,
+        );
     }
 }
