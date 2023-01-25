f2aadc30b8268ac92d6a51ec81748c5c765b47f3
brainshaker <parupp@gmx.de>
2023-01-25 20:11:25 +0100

    feat(doc-comments): add array nodes

---
+++ src/Model/Ast/Type/ArrayShapeItemNode.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Brainshaker95\PhpToTsBundle\Model\Ast\Type;
+
+use Brainshaker95\PhpToTsBundle\Interface\Node;
+use Brainshaker95\PhpToTsBundle\Model\Config\Indent;
+use Brainshaker95\PhpToTsBundle\Tool\Type;
+use PHPStan\PhpDocParser\Ast\Node as PHPStanNode;
+use PHPStan\PhpDocParser\Ast\Type\ArrayShapeItemNode as PHPStanArrayShapeItemNode;
+
+class ArrayShapeItemNode implements Node
+{
+    private ?Indent $indent = null;
+
+    public function __construct(
+        public readonly ?Node $keyNode,
+        public readonly bool $isOptional,
+        public readonly Node $valueNode,
+    ) {
+    }
+
+    public function __toString(): string
+    {
+        return $this->toString();
+    }
+
+    public function toString(): string
+    {
+        if (!$this->keyNode) {
+            return sprintf(
+                '%s%s,' . PHP_EOL,
+                $this->indent?->toString() ?? '',
+                (string) $this->valueNode,
+            );
+        }
+
+        $key = (string) $this->keyNode;
+
+        return sprintf(
+            '%s%s%s: %s;' . PHP_EOL,
+            $this->indent?->toString() ?? '',
+            // TODO: config for quote style ' or "
+            is_numeric($key[0]) ? '\'' . $key . '\'' : $key,
+            $this->isOptional ? '?' : '',
+            (string) $this->valueNode,
+        );
+    }
+
+    /**
+     * @param PHPStanArrayShapeItemNode $node
+     */
+    public static function fromPhpStan(PHPStanNode $node): self
+    {
+        return new self(
+            keyNode: $node->keyName ? Type::fromPhpStan($node->keyName) : null,
+            isOptional: $node->optional,
+            valueNode: Type::fromPhpStan($node->valueType),
+        );
+    }
+
+    public function setIndent(?Indent $indent): self
+    {
+        $this->indent = $indent;
+
+        return $this;
+    }
+}
---
+++ src/Model/Ast/Type/ArrayShapeNode.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Brainshaker95\PhpToTsBundle\Model\Ast\Type;
+
+use Brainshaker95\PhpToTsBundle\Interface\Node;
+use Brainshaker95\PhpToTsBundle\Model\Config\Indent;
+use PHPStan\PhpDocParser\Ast\Node as PHPStanNode;
+use PHPStan\PhpDocParser\Ast\Type\ArrayShapeNode as PHPStanArrayShapeNode;
+
+class ArrayShapeNode implements Node
+{
+    private ?Indent $indent = null;
+
+    /**
+     * @param ArrayShapeItemNode[] $items
+     */
+    public function __construct(
+        public readonly array $items,
+    ) {
+    }
+
+    public function __toString(): string
+    {
+        return $this->toString();
+    }
+
+    public function toString(): string
+    {
+        $hasKeys        = (bool) current(array_filter($this->items, fn (ArrayShapeItemNode $node) => $node->keyNode));
+        $openingBracket = $hasKeys ? '{' : '[';
+        $closingBracket = $hasKeys ? '}' : ']';
+
+        return $openingBracket . PHP_EOL
+            . implode('', $this->items)
+            . ($this->indent?->toString() ?? '') . $closingBracket;
+    }
+
+    /**
+     * @param PHPStanArrayShapeNode $node
+     */
+    public static function fromPhpStan(PHPStanNode $node): self
+    {
+        return new self(
+            items: array_map(
+                [ArrayShapeItemNode::class, 'fromPhpStan'],
+                $node->items,
+            ),
+        );
+    }
+
+    public function setIndent(?Indent $indent): self
+    {
+        $this->indent = $indent;
+
+        return $this;
+    }
+}
