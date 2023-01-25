1a12ffa38980a55d6339b0da55ffe6af515a2e3b
brainshaker <parupp@gmx.de>
2023-01-25 22:20:45 +0100

    refactor(phpstan-tool): minify file length by optimizing imports

--- src/Tool/PhpStan.php
+++ src/Tool/PhpStan.php
@@ -4,40 +4,19 @@ namespace Brainshaker95\PhpToTsBundle\Tool;
 
 use Brainshaker95\PhpToTsBundle\Exception\UnsupportedNodeException;
 use Brainshaker95\PhpToTsBundle\Interface\Node;
-use Brainshaker95\PhpToTsBundle\Model\Ast\ConstExpr\ConstExprFalseNode;
-use Brainshaker95\PhpToTsBundle\Model\Ast\ConstExpr\ConstExprFloatNode;
-use Brainshaker95\PhpToTsBundle\Model\Ast\ConstExpr\ConstExprIntegerNode;
-use Brainshaker95\PhpToTsBundle\Model\Ast\ConstExpr\ConstExprNullNode;
-use Brainshaker95\PhpToTsBundle\Model\Ast\ConstExpr\ConstExprStringNode;
-use Brainshaker95\PhpToTsBundle\Model\Ast\ConstExpr\ConstExprTrueNode;
-use Brainshaker95\PhpToTsBundle\Model\Ast\ConstExpr\ConstFetchNode;
-use Brainshaker95\PhpToTsBundle\Model\Ast\Type\ArrayShapeItemNode;
-use Brainshaker95\PhpToTsBundle\Model\Ast\Type\ArrayShapeNode;
-use Brainshaker95\PhpToTsBundle\Model\Ast\Type\ArrayTypeNode;
-use Brainshaker95\PhpToTsBundle\Model\Ast\Type\ConstTypeNode;
-use Brainshaker95\PhpToTsBundle\Model\Ast\Type\GenericTypeNode;
-use Brainshaker95\PhpToTsBundle\Model\Ast\Type\IdentifierTypeNode;
-use Brainshaker95\PhpToTsBundle\Model\Ast\Type\IntersectionTypeNode;
-use Brainshaker95\PhpToTsBundle\Model\Ast\Type\NullableTypeNode;
-use Brainshaker95\PhpToTsBundle\Model\Ast\Type\UnionTypeNode;
+use Brainshaker95\PhpToTsBundle\Model\Ast\ConstExpr;
+use Brainshaker95\PhpToTsBundle\Model\Ast\Type;
 use Closure;
-use PHPStan\PhpDocParser\Ast\ConstExpr\ConstExprFalseNode as PHPStanConstExprFalseNode;
-use PHPStan\PhpDocParser\Ast\ConstExpr\ConstExprFloatNode as PHPStanConstExprFloatNode;
-use PHPStan\PhpDocParser\Ast\ConstExpr\ConstExprIntegerNode as PHPStanConstExprIntegerNode;
-use PHPStan\PhpDocParser\Ast\ConstExpr\ConstExprNullNode as PHPStanConstExprNullNode;
-use PHPStan\PhpDocParser\Ast\ConstExpr\ConstExprStringNode as PHPStanConstExprStringNode;
-use PHPStan\PhpDocParser\Ast\ConstExpr\ConstExprTrueNode as PHPStanConstExprTrueNode;
-use PHPStan\PhpDocParser\Ast\ConstExpr\ConstFetchNode as PHPStanConstFetchNode;
+use PhpParser\Comment\Doc;
+use PHPStan\PhpDocParser\Ast\ConstExpr as PHPStanConstExpr;
 use PHPStan\PhpDocParser\Ast\Node as PHPStanNode;
-use PHPStan\PhpDocParser\Ast\Type\ArrayShapeItemNode as PHPStanArrayShapeItemNode;
-use PHPStan\PhpDocParser\Ast\Type\ArrayShapeNode as PHPStanArrayShapeNode;
-use PHPStan\PhpDocParser\Ast\Type\ArrayTypeNode as PHPStanArrayTypeNode;
-use PHPStan\PhpDocParser\Ast\Type\ConstTypeNode as PHPStanConstTypeNode;
-use PHPStan\PhpDocParser\Ast\Type\GenericTypeNode as PHPStanGenericTypeNode;
-use PHPStan\PhpDocParser\Ast\Type\IdentifierTypeNode as PHPStanIdentifierTypeNode;
-use PHPStan\PhpDocParser\Ast\Type\IntersectionTypeNode as PHPStanIntersectionTypeNode;
-use PHPStan\PhpDocParser\Ast\Type\NullableTypeNode as PHPStanNullableTypeNode;
-use PHPStan\PhpDocParser\Ast\Type\UnionTypeNode as PHPStanUnionTypeNode;
+use PHPStan\PhpDocParser\Ast\PhpDoc\DeprecatedTagValueNode;
+use PHPStan\PhpDocParser\Ast\PhpDoc\ParamTagValueNode;
+use PHPStan\PhpDocParser\Ast\PhpDoc\PhpDocNode;
+use PHPStan\PhpDocParser\Ast\PhpDoc\TemplateTagValueNode;
+use PHPStan\PhpDocParser\Ast\PhpDoc\TypelessParamTagValueNode;
+use PHPStan\PhpDocParser\Ast\PhpDoc\VarTagValueNode;
+use PHPStan\PhpDocParser\Ast\Type as PHPStanType;
 
 abstract class PhpStan
 {
@@ -45,22 +24,22 @@ abstract class PhpStan
      * @var array<class-string<PHPStanNode>,class-string<Node>>
      */
     public const NODE_CLASS_MAP = [
-        PHPStanArrayShapeItemNode::class   => ArrayShapeItemNode::class,
-        PHPStanArrayShapeNode::class       => ArrayShapeNode::class,
-        PHPStanArrayTypeNode::class        => ArrayTypeNode::class,
-        PHPStanConstTypeNode::class        => ConstTypeNode::class,
-        PHPStanGenericTypeNode::class      => GenericTypeNode::class,
-        PHPStanIdentifierTypeNode::class   => IdentifierTypeNode::class,
-        PHPStanIntersectionTypeNode::class => IntersectionTypeNode::class,
-        PHPStanNullableTypeNode::class     => NullableTypeNode::class,
-        PHPStanUnionTypeNode::class        => UnionTypeNode::class,
-        PHPStanConstExprTrueNode::class    => ConstExprTrueNode::class,
-        PHPStanConstExprFalseNode::class   => ConstExprFalseNode::class,
-        PHPStanConstExprFloatNode::class   => ConstExprFloatNode::class,
-        PHPStanConstExprIntegerNode::class => ConstExprIntegerNode::class,
-        PHPStanConstExprNullNode::class    => ConstExprNullNode::class,
-        PHPStanConstExprStringNode::class  => ConstExprStringNode::class,
-        PHPStanConstFetchNode::class       => ConstFetchNode::class,
+        PHPStanConstExpr\ConstExprFalseNode::class   => ConstExpr\ConstExprFalseNode::class,
+        PHPStanConstExpr\ConstExprFloatNode::class   => ConstExpr\ConstExprFloatNode::class,
+        PHPStanConstExpr\ConstExprIntegerNode::class => ConstExpr\ConstExprIntegerNode::class,
+        PHPStanConstExpr\ConstExprNullNode::class    => ConstExpr\ConstExprNullNode::class,
+        PHPStanConstExpr\ConstExprStringNode::class  => ConstExpr\ConstExprStringNode::class,
+        PHPStanConstExpr\ConstExprTrueNode::class    => ConstExpr\ConstExprTrueNode::class,
+        PHPStanConstExpr\ConstFetchNode::class       => ConstExpr\ConstFetchNode::class,
+        PHPStanType\ArrayShapeItemNode::class        => Type\ArrayShapeItemNode::class,
+        PHPStanType\ArrayShapeNode::class            => Type\ArrayShapeNode::class,
+        PHPStanType\ArrayTypeNode::class             => Type\ArrayTypeNode::class,
+        PHPStanType\ConstTypeNode::class             => Type\ConstTypeNode::class,
+        PHPStanType\GenericTypeNode::class           => Type\GenericTypeNode::class,
+        PHPStanType\IdentifierTypeNode::class        => Type\IdentifierTypeNode::class,
+        PHPStanType\IntersectionTypeNode::class      => Type\IntersectionTypeNode::class,
+        PHPStanType\NullableTypeNode::class          => Type\NullableTypeNode::class,
+        PHPStanType\UnionTypeNode::class             => Type\UnionTypeNode::class,
     ];
 
     public static function toNode(PHPStanNode $node): Node
