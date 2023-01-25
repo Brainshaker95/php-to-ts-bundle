87d49445bffed80d34df3dbce1ad7064b23f3e81
brainshaker <parupp@gmx.de>
2023-01-25 20:12:09 +0100

    feat(doc-comments): add generic node

---
+++ src/Exception/UnsupportedNodeException.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Brainshaker95\PhpToTsBundle\Exception;
+
+use RuntimeException;
+
+class UnsupportedNodeException extends RuntimeException
+{
+}
---
+++ src/Model/Ast/Type/GenericTypeNode.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Brainshaker95\PhpToTsBundle\Model\Ast\Type;
+
+use Brainshaker95\PhpToTsBundle\Exception\AssertionFailedException;
+use Brainshaker95\PhpToTsBundle\Exception\UnsupportedNodeException;
+use Brainshaker95\PhpToTsBundle\Interface\Node;
+use Brainshaker95\PhpToTsBundle\Tool\Converter;
+use Brainshaker95\PhpToTsBundle\Tool\Type;
+use PHPStan\PhpDocParser\Ast\Node as PHPStanNode;
+use PHPStan\PhpDocParser\Ast\Type\GenericTypeNode as PHPStanGenericTypeNode;
+
+class GenericTypeNode implements Node
+{
+    /**
+     * @param Node[] $genericTypes
+     */
+    public function __construct(
+        public readonly IdentifierTypeNode $type,
+        public readonly array $genericTypes,
+    ) {
+    }
+
+    public function __toString(): string
+    {
+        return $this->toString();
+    }
+
+    public function toString(): string
+    {
+        $type = $this->type->name;
+
+        if ($type === 'Array') {
+            $genericTypeCount = count($this->genericTypes);
+
+            if ($genericTypeCount === 2) {
+                $type = 'Record';
+            } elseif ($genericTypeCount !== 1) {
+                throw new UnsupportedNodeException(sprintf(
+                    'Expected generic array node to contain 1 or 2 subtypes, %s given.',
+                    $genericTypeCount,
+                ));
+            }
+        } elseif (array_key_exists($type, array_flip(Converter::NON_ITERABLE_TYPE_MAP))) {
+            return $type;
+        }
+
+        return $type . '<' . implode(', ', $this->genericTypes) . '>';
+    }
+
+    /**
+     * @param PHPStanGenericTypeNode $node
+     */
+    public static function fromPhpStan(PHPStanNode $node): self
+    {
+        foreach ($node->variances as $variance) {
+            if ($variance !== PHPStanGenericTypeNode::VARIANCE_INVARIANT) {
+                throw new UnsupportedNodeException(sprintf(
+                    'Invalid variance "%s" of generic type node. Only %s nodes are supported.',
+                    $variance,
+                    PHPStanGenericTypeNode::VARIANCE_INVARIANT,
+                ));
+            }
+        }
+
+        $type = Type::fromPhpStan($node->type);
+
+        if (!$type instanceof IdentifierTypeNode) {
+            throw new AssertionFailedException(sprintf(
+                'Expected node to be an instance of "%s", "%s" given.',
+                IdentifierTypeNode::class,
+                get_class($type),
+            ));
+        }
+
+        return new self(
+            type: $type,
+            genericTypes: array_map(
+                [Type::class, 'fromPhpStan'],
+                $node->genericTypes,
+            ),
+        );
+    }
+}
