dd964a055771278f14c0b1c1bc84418b462cc462
brainshaker <parupp@gmx.de>
2023-02-23 18:00:58 +0100

    test(configuration): add test for merge function

--- tests/ConfigurationTest.php
+++ tests/ConfigurationTest.php
@@ -5,6 +5,11 @@ declare(strict_types=1);
 namespace App\Tests;
 
 use Brainshaker95\PhpToTsBundle\Interface\Config as C;
+use Brainshaker95\PhpToTsBundle\Model\Config\FileType;
+use Brainshaker95\PhpToTsBundle\Model\Config\Indent;
+use Brainshaker95\PhpToTsBundle\Model\Config\PartialConfig;
+use Brainshaker95\PhpToTsBundle\Model\Config\SortStrategy\AlphabeticalAsc;
+use Brainshaker95\PhpToTsBundle\Model\Config\SortStrategy\ReadonlyLast;
 use Brainshaker95\PhpToTsBundle\Service\Configuration;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
@@ -56,6 +61,33 @@ final class ConfigurationTest extends KernelTestCase
         self::assertSame($this->phpToTs[C::INDENT_KEY][C::INDENT_COUNT_KEY], $config->getIndent()->count);
         self::assertSame($this->phpToTs[C::QUOTES_KEY], $config->getQuotes()->style);
         self::assertTrue(count(array_diff($this->phpToTs[C::SORT_STRATEGIES_KEY], $config->getSortStrategies())) === 0);
+        self::assertSame($this->phpToTs[C::FILE_NAME_STRATEGY_KEY], $config->getFileNameStrategy());
+    }
+
+    public function testThatTheConfigurationCanBeMergedWithOther(): void
+    {
+        $fileType = FileType::TYPE_DECLARATION;
+        $indent   = new Indent(Indent::STYLE_TAB);
+
+        $sortStrategies = [
+            AlphabeticalAsc::class,
+            ReadonlyLast::class,
+        ];
+
+        $config = $this->config->merge(new PartialConfig(
+            fileType: $fileType,
+            indent: $indent,
+            sortStrategies: $sortStrategies,
+        ));
+
+        self::assertSame($this->phpToTs[C::INPUT_DIR_KEY], $config->getInputDir());
+        self::assertSame($this->phpToTs[C::OUTPUT_DIR_KEY], $config->getOutputDir());
+        self::assertSame($fileType, $config->getFileType());
+        self::assertSame($this->phpToTs[C::TYPE_DEFINITION_TYPE_KEY], $config->getTypeDefinitionType());
+        self::assertSame($indent->style, $config->getIndent()->style);
+        self::assertSame($this->phpToTs[C::INDENT_KEY][C::INDENT_COUNT_KEY], $config->getIndent()->count);
+        self::assertSame($this->phpToTs[C::QUOTES_KEY], $config->getQuotes()->style);
+        self::assertTrue(count(array_diff($sortStrategies, $config->getSortStrategies())) === 0);
         self::assertSame($this->phpToTs[C::FILE_NAME_STRATEGY_KEY], $config->getFileNameStrategy());
     }
 }
