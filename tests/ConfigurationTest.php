7573444a00203c5b3c39d165a99c78ef311faf15
brainshaker <parupp@gmx.de>
2023-03-06 21:01:21 +0100

    test(configuration): add additional tests to cover remaining methods

--- tests/ConfigurationTest.php
+++ tests/ConfigurationTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Tests;
 
 use Brainshaker95\PhpToTsBundle\Interface\Config as C;
+use Brainshaker95\PhpToTsBundle\Model\Config\FileNameStrategy\PascalCase;
 use Brainshaker95\PhpToTsBundle\Model\Config\FileType;
 use Brainshaker95\PhpToTsBundle\Model\Config\FullConfig;
 use Brainshaker95\PhpToTsBundle\Model\Config\Indent;
@@ -12,6 +13,7 @@ use Brainshaker95\PhpToTsBundle\Model\Config\PartialConfig;
 use Brainshaker95\PhpToTsBundle\Model\Config\Quotes;
 use Brainshaker95\PhpToTsBundle\Model\Config\SortStrategy\AlphabeticalAsc;
 use Brainshaker95\PhpToTsBundle\Model\Config\SortStrategy\ReadonlyLast;
+use Brainshaker95\PhpToTsBundle\Model\Config\TypeDefinitionType;
 use Brainshaker95\PhpToTsBundle\Service\Configuration;
 use ReflectionClass;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
@@ -24,6 +26,8 @@ use function count;
  *
  * @covers \Brainshaker95\PhpToTsBundle\DependencyInjection\Configuration
  * @covers \Brainshaker95\PhpToTsBundle\DependencyInjection\PhpToTsExtension
+ * @covers \Brainshaker95\PhpToTsBundle\Model\Config\FullConfig
+ * @covers \Brainshaker95\PhpToTsBundle\Model\Config\PartialConfig
  * @covers \Brainshaker95\PhpToTsBundle\Service\Configuration
  *
  * @phpstan-import-type ConfigurationArray from Configuration
@@ -122,5 +126,63 @@ final class ConfigurationTest extends KernelTestCase
         $config = $this->config->merge(null);
 
         self::assertSame($this->config->get(), $config);
+    }
+
+    public function testConfigCreationFromArray(): void
+    {
+        $fullConfig     = FullConfig::fromArray($this->phpToTs);
+        $partialConfig1 = PartialConfig::fromArray($this->phpToTs);
+        $partialConfig2 = PartialConfig::fromArray([]);
+
+        self::assertInstanceOf(FullConfig::class, $fullConfig);
+        self::assertInstanceOf(PartialConfig::class, $partialConfig1);
+        self::assertInstanceOf(PartialConfig::class, $partialConfig2);
+
+        self::assertSame($this->phpToTs[C::INPUT_DIR_KEY], $partialConfig1->getInputDir());
+        self::assertSame($this->phpToTs[C::OUTPUT_DIR_KEY], $partialConfig1->getOutputDir());
+        self::assertSame($this->phpToTs[C::FILE_TYPE_KEY], $partialConfig1->getFileType());
+        self::assertSame($this->phpToTs[C::TYPE_DEFINITION_TYPE_KEY], $partialConfig1->getTypeDefinitionType());
+        self::assertSame($this->phpToTs[C::INDENT_KEY][C::INDENT_STYLE_KEY], $partialConfig1->getIndent()?->style);
+        self::assertSame($this->phpToTs[C::INDENT_KEY][C::INDENT_COUNT_KEY], $partialConfig1->getIndent()->count);
+        self::assertSame($this->phpToTs[C::QUOTES_KEY], $partialConfig1->getQuotes()?->style);
+        self::assertSame($this->phpToTs[C::SORT_STRATEGIES_KEY], $partialConfig1->getSortStrategies());
+        self::assertSame($this->phpToTs[C::FILE_NAME_STRATEGY_KEY], $partialConfig1->getFileNameStrategy());
+
+        self::assertNull($partialConfig2->getInputDir());
+        self::assertNull($partialConfig2->getOutputDir());
+        self::assertNull($partialConfig2->getFileType());
+        self::assertNull($partialConfig2->getTypeDefinitionType());
+        self::assertNull($partialConfig2->getIndent());
+        self::assertNull($partialConfig2->getIndent());
+        self::assertNull($partialConfig2->getQuotes());
+        self::assertNull($partialConfig2->getSortStrategies());
+        self::assertNull($partialConfig2->getFileNameStrategy());
+    }
+
+    public function testSettingOfConfigValues(): void
+    {
+        $baseConfig = $this->config->get();
+
+        foreach ([clone $baseConfig, new PartialConfig()] as $otherConfig) {
+            $otherConfig
+                ->setInputDir('Test')
+                ->setOutputDir('Test')
+                ->setFileType(FileType::TYPE_DECLARATION)
+                ->setTypeDefinitionType(TypeDefinitionType::TYPE_TYPE_ALIAS)
+                ->setIndent(new Indent(Indent::STYLE_TAB, 1))
+                ->setQuotes(new Quotes(Quotes::STYLE_DOUBLE))
+                ->setSortStrategies([AlphabeticalAsc::class])
+                ->setFileNameStrategy(PascalCase::class)
+            ;
+
+            self::assertNotSame($baseConfig->getInputDir(), $otherConfig->getInputDir());
+            self::assertNotSame($baseConfig->getOutputDir(), $otherConfig->getOutputDir());
+            self::assertNotSame($baseConfig->getFileType(), $otherConfig->getFileType());
+            self::assertNotSame($baseConfig->getTypeDefinitionType(), $otherConfig->getTypeDefinitionType());
+            self::assertNotSame($baseConfig->getIndent(), $otherConfig->getIndent());
+            self::assertNotSame($baseConfig->getQuotes(), $otherConfig->getQuotes());
+            self::assertNotSame($baseConfig->getSortStrategies(), $otherConfig->getSortStrategies());
+            self::assertNotSame($baseConfig->getFileNameStrategy(), $otherConfig->getFileNameStrategy());
+        }
     }
 }
