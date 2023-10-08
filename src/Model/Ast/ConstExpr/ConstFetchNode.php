2445f371d288453073642f72d8c8f7376abd86e3
brainshaker <parupp@gmx.de>
2023-10-08 21:39:29 +0200

    feat(nodes): add support for more types to ConstFetchNode

--- src/Model/Ast/ConstExpr/ConstFetchNode.php
+++ src/Model/Ast/ConstExpr/ConstFetchNode.php
@@ -6,7 +6,6 @@ namespace Brainshaker95\PhpToTsBundle\Model\Ast\ConstExpr;
 
 use Brainshaker95\PhpToTsBundle\Interface\Node;
 use Brainshaker95\PhpToTsBundle\Interface\Quotable;
-use Brainshaker95\PhpToTsBundle\Model\Config\Quotes;
 use Brainshaker95\PhpToTsBundle\Model\Traits\HasQuotes;
 use Brainshaker95\PhpToTsBundle\Model\TsProperty;
 use Brainshaker95\PhpToTsBundle\Tool\Assert;
@@ -14,8 +13,13 @@ use Error;
 use PHPStan\PhpDocParser\Ast\ConstExpr\ConstFetchNode as PHPStanConstFetchNode;
 use PHPStan\PhpDocParser\Ast\Node as PHPStanNode;
 
+use function array_is_list;
 use function constant;
+use function implode;
 use function is_array;
+use function is_bool;
+use function is_float;
+use function is_int;
 use function is_string;
 
 /**
@@ -47,13 +51,7 @@ final class ConstFetchNode implements Node, Quotable
             return TsProperty::TYPE_UNKNOWN;
         }
 
-        if (is_string($value)) {
-            return $this->quotes
-                ? $this->quotes->toString($value)
-                : Quotes::default($value);
-        }
-
-        return TsProperty::TYPE_UNKNOWN . (is_array($value) ? '[]' : '');
+        return $this->valueToString($value);
     }
 
     public static function fromPhpStan(PHPStanNode $node): self
@@ -64,5 +62,63 @@ final class ConstFetchNode implements Node, Quotable
             className: $node->className,
             name: $node->name,
         );
+    }
+
+    private function valueToString(mixed $value): string
+    {
+        if ($value === null) {
+            return (new ConstExprNullNode())->toString();
+        }
+
+        if (is_string($value)) {
+            $node = (new ConstExprStringNode($value));
+
+            if ($this->quotes) {
+                $node->setQuotes($this->quotes);
+            }
+
+            return $node->toString();
+        }
+
+        if (is_bool($value)) {
+            return $value
+                ? (new ConstExprTrueNode())->toString()
+                : (new ConstExprFalseNode())->toString();
+        }
+
+        if (is_int($value)) {
+            return (new ConstExprIntegerNode((string) $value))->toString();
+        }
+
+        if (is_float($value)) {
+            return (new ConstExprFloatNode((string) $value))->toString();
+        }
+
+        if (!is_array($value)) {
+            return TsProperty::TYPE_UNKNOWN;
+        }
+
+        $hasKeys        = !array_is_list($value);
+        $openingBracket = $hasKeys ? '{ ' : '[';
+        $closingBracket = $hasKeys ? ' }' : ']';
+        $values         = [];
+
+        foreach ($value as $key => $item) {
+            try {
+                $itemValue = constant($item);
+            } catch (Error) {
+                $itemValue = $item;
+            }
+
+            if ($hasKeys) {
+                $values[] = $key . ': ' . $this->valueToString($itemValue);
+            } else {
+                $values[] = $this->valueToString($itemValue);
+            }
+        }
+
+        return $openingBracket
+            . implode(', ', $values)
+            . $closingBracket;
     }
 }
