c5f69ac2a854fecc7a4443de83f7ff13e117aed6
brainshaker <parupp@gmx.de>
2023-10-08 20:45:57 +0200

    test(ts-controller): rename SerializerTest and also test TsController logic

--- tests/TsControllerTest.php
+++ tests/TsControllerTest.php
@@ -4,9 +4,12 @@ declare(strict_types=1);
 
 namespace App\Tests;
 
+use Brainshaker95\PhpToTsBundle\Attribute\AsTypeScriptable;
 use Brainshaker95\PhpToTsBundle\Serializer\Serializer;
-use Brainshaker95\PhpToTsBundle\Service\Traits\HasSerializer;
+use Brainshaker95\PhpToTsBundle\Service\Traits\TsController;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * @internal
@@ -15,10 +18,11 @@ use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
  *
  * @covers \Brainshaker95\PhpToTsBundle\Serializer\Serializer
  * @covers \Brainshaker95\PhpToTsBundle\Service\Traits\HasSerializer
+ * @covers \Brainshaker95\PhpToTsBundle\Service\Traits\TsController
  */
-final class SeializerTest extends KernelTestCase
+final class TsControllerTest extends KernelTestCase
 {
-    use HasSerializer;
+    use TsController;
 
     protected function setUp(): void
     {
@@ -30,9 +34,9 @@ final class SeializerTest extends KernelTestCase
         $this->setSerializer($serializer);
     }
 
-    public function testSerializer(): void
+    public function testTsController(): void
     {
-        $instance = new class(true, ['foo' => ['bar' => ['baz']]]) {
+        $instance = new #[AsTypeScriptable] class(true, ['foo' => ['bar' => ['baz']]]) {
             public int $property1;
 
             public string $property2;
@@ -49,9 +53,21 @@ final class SeializerTest extends KernelTestCase
         $instance->property1 = 1;
         $instance->property2 = '1';
 
+        $response = $this->ts($instance);
+
+        self::assertTrue($response->getStatusCode() === Response::HTTP_OK);
+        self::assertInstanceOf(JsonResponse::class, $response);
+
+        $expected = '{"property1":1,"property2":"1","property3":true,"property4":{"foo":{"bar":["baz"]}}}';
+
         self::assertSame(
-            expected: '{"property1":1,"property2":"1","property3":true,"property4":{"foo":{"bar":["baz"]}}}',
+            expected: $expected,
             actual: $this->serializer->serialize($instance, 'json'),
+        );
+
+        self::assertSame(
+            expected: $expected,
+            actual: $response->getContent(),
         );
     }
 }
