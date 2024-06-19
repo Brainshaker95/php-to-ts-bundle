19d3b4b153a05ea0b9da8f71ec51e3b6f3c11451
brainshaker <parupp@gmx.de>
2024-06-19 20:29:44 +0200

    chore(dependencies): update dev-dependencies

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,4 +2,10 @@
 
 declare(strict_types=1);
 
+use Symfony\Component\ErrorHandler\ErrorHandler;
+
 require \dirname(__DIR__) . '/vendor/autoload.php';
+
+// TODO: Follow this issue and remove this hack someday
+// See: https://github.com/symfony/symfony/issues/53812#issuecomment-1962740145
+\set_exception_handler([new ErrorHandler(), 'handleException']);
