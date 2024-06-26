f1ab1fed1abbbb948858104a498753b8fd3f05ca
brainshaker <parupp@gmx.de>
2024-06-26 21:31:48 +0200

    feat(nodes): add wildcard support to ConstFetchNode

--- src/Model/Ast/ConstExpr/ConstFetchNode.php
+++ src/Model/Ast/ConstExpr/ConstFetchNode.php
@@ -17,9 +17,20 @@ use Brainshaker95\PhpToTsBundle\Tool\PhpStan;
 use Error;
 use PHPStan\PhpDocParser\Ast\ConstExpr\ConstFetchNode as PHPStanConstFetchNode;
 use PHPStan\PhpDocParser\Ast\Node as PHPStanNode;
+use ReflectionClass;
+use ReflectionClassConstant;
 use Stringable;
 
+use const ARRAY_FILTER_USE_KEY;
+
+use function array_filter;
+use function array_keys;
+use function array_map;
+use function class_exists;
 use function constant;
+use function fnmatch;
+use function implode;
+use function Symfony\Component\String\u;
 
 /**
  * @internal
@@ -41,19 +52,44 @@ final class ConstFetchNode implements Indentable, Node, Quotable, Stringable
 
     public function toString(): string
     {
-        try {
-            $value = $this->className
-                ? constant($this->className . '::' . $this->name)
-                : constant($this->name);
-        } catch (Error) {
-            return TsProperty::TYPE_UNKNOWN;
+        $hasWildcard = u($this->name)->indexOf('*') !== null;
+        $names       = $hasWildcard ? [] : [$this->name];
+        $values      = [];
+
+        if ($hasWildcard) {
+            if (!$this->className || !class_exists($this->className)) {
+                return TsProperty::TYPE_UNKNOWN;
+            }
+
+            $constants = (new ReflectionClass($this->className))->getConstants(
+                ReflectionClassConstant::IS_PUBLIC,
+            );
+
+            $names = array_keys(array_filter(
+                $constants,
+                fn (string $key): bool => fnmatch($this->name, $key),
+                ARRAY_FILTER_USE_KEY,
+            ));
         }
 
-        return PhpStan::phpValueToTsType(
-            $value,
-            $this->indent ?? new Indent(),
-            $this->quotes ?? new Quotes(),
-        );
+        foreach ($names as $name) {
+            try {
+                $values[] = $this->className
+                    ? constant($this->className . '::' . $name)
+                    : constant($name);
+            } catch (Error) {
+                return TsProperty::TYPE_UNKNOWN;
+            }
+        }
+
+        return implode(' | ', array_map(
+            fn (mixed $value): string => PhpStan::phpValueToTsType(
+                $value,
+                $this->indent ?? new Indent(),
+                $this->quotes ?? new Quotes(),
+            ),
+            $values,
+        )) ?: TsProperty::TYPE_UNKNOWN;
     }
 
     public static function fromPhpStan(PHPStanNode $node): self
