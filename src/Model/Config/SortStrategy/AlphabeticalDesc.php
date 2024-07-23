544643d77e1ebb2082e650a921d4437dba1d9a32
brainshaker <parupp@gmx.de>
2024-07-23 21:23:02 +0200

    feat(file-name-strategy): use strnatcasecmp function for alphabetical strategies

--- src/Model/Config/SortStrategy/AlphabeticalDesc.php
+++ src/Model/Config/SortStrategy/AlphabeticalDesc.php
@@ -7,10 +7,12 @@ namespace Brainshaker95\PhpToTsBundle\Model\Config\SortStrategy;
 use Brainshaker95\PhpToTsBundle\Interface\SortStrategy;
 use Brainshaker95\PhpToTsBundle\Model\TsProperty;
 
+use function strnatcasecmp;
+
 final class AlphabeticalDesc implements SortStrategy
 {
     public function sort(TsProperty $property1, TsProperty $property2): int
     {
-        return $property2->name <=> $property1->name;
+        return strnatcasecmp($property2->name, $property1->name);
     }
 }
