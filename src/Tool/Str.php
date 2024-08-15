bfc0eddd0872be75b80a2b977845bbd938bae18c
brainshaker <parupp@gmx.de>
2024-08-15 23:02:11 +0200

    fix(phpstan): make string tool final instead of abstract

--- src/Tool/Str.php
+++ src/Tool/Str.php
@@ -24,14 +24,14 @@ use function Symfony\Component\String\u;
 /**
  * @internal
  */
-abstract class Str
+final class Str
 {
     /**
      * @codeCoverageIgnore
      */
     private function __construct() {}
 
-    final public static function toLower(string $string): string
+    public static function toLower(string $string): string
     {
         return u($string)
             ->lower()
@@ -39,7 +39,7 @@ abstract class Str
         ;
     }
 
-    final public static function toUpper(string $string): string
+    public static function toUpper(string $string): string
     {
         return u($string)
             ->upper()
@@ -47,7 +47,7 @@ abstract class Str
         ;
     }
 
-    final public static function toCamel(string $string): string
+    public static function toCamel(string $string): string
     {
         return u($string)
             ->camel()
@@ -55,7 +55,7 @@ abstract class Str
         ;
     }
 
-    final public static function toPascal(string $string): string
+    public static function toPascal(string $string): string
     {
         return u($string)
             ->camel()
@@ -64,7 +64,7 @@ abstract class Str
         ;
     }
 
-    final public static function toSnake(string $string): string
+    public static function toSnake(string $string): string
     {
         return u($string)
             ->snake()
@@ -72,7 +72,7 @@ abstract class Str
         ;
     }
 
-    final public static function toKebab(string $string): string
+    public static function toKebab(string $string): string
     {
         return u($string)
             ->snake()
@@ -84,12 +84,12 @@ abstract class Str
     /**
      * @phpstan-assert-if-true !non-empty-string $string
      */
-    final public static function isEmpty(?string $string): bool
+    public static function isEmpty(?string $string): bool
     {
         return $string === null || u($string)->trim()->length() === 0;
     }
 
-    final public static function trimEnd(
+    public static function trimEnd(
         string $string,
         string $additionalChars = '',
         string $chars = " \t\n\r\0\x0B\x0C\u{A0}\u{FEFF}",
@@ -100,7 +100,7 @@ abstract class Str
         ;
     }
 
-    final public static function indentAndPrefixLines(string $string, ?Indent $indent, string $prefix = ''): string
+    public static function indentAndPrefixLines(string $string, ?Indent $indent, string $prefix = ''): string
     {
         $lines = u($string)
             ->replace("\r\n", "\n")
@@ -116,7 +116,7 @@ abstract class Str
         ));
     }
 
-    final public static function containsNewlines(string $string): bool
+    public static function containsNewlines(string $string): bool
     {
         return u($string)
             ->replace("\r\n", "\n")
@@ -125,7 +125,7 @@ abstract class Str
         ;
     }
 
-    final public static function displayType(mixed $value): string
+    public static function displayType(mixed $value): string
     {
         if ($value === null) {
             return Converter::TYPE_NULL;
@@ -165,21 +165,21 @@ abstract class Str
         return Converter::TYPE_MIXED;
     }
 
-    final public static function getShortClassName(string $className): string
+    public static function getShortClassName(string $className): string
     {
         return str_contains($className, '\\')
             ? u($className)->afterLast('\\')->toString()
             : $className;
     }
 
-    final public static function getFirstNamespacePart(string $className): ?string
+    public static function getFirstNamespacePart(string $className): ?string
     {
         return str_contains($className, '\\')
             ? u($className)->before('\\')->toString()
             : null;
     }
 
-    final public static function getTrailingNamespaceParts(string $className): ?string
+    public static function getTrailingNamespaceParts(string $className): ?string
     {
         return str_contains($className, '\\')
             ? u($className)->after('\\')->toString()
